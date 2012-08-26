ca4fc20b39eccbab9d2df7e8d78374f8211c4d7e
Steven Robertson <steve@playnode.com>
2012-08-26 10:22:46 +0100

    Simple timer/timeout example.

---
+++ timeout/timeout.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("5")
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("4")
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("3")
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("2")
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("1")
+}
