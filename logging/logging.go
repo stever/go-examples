9cba1fce38fac048896d339cab063d265162795d
Steven Robertson <steve@playnode.com>
2012-10-28 08:51:07 +0000

    Optional debugging example.

--- logging/logging.go
+++ logging/logging.go
@@ -1,8 +1,21 @@
+// Logging example.
+// http://play.golang.org/p/QFheQeChIn
+// https://groups.google.com/d/topic/golang-nuts/gU7oQGoCkmg/discussion
+
 package main
 
 import "log"
 
+const debug debugging = true // or flip to false
+
+type debugging bool
+
+func (d debugging) Printf(format string, args ...interface{}) {
+	if d {
+		log.Printf(format, args...)
+	}
+}
+
 func main() {
-	log.Println("This is a log statement.")
-	log.Fatalln("Fatal also exits.")
+	debug.Printf("foo %d %.2f", 42, 12.7)
 }
